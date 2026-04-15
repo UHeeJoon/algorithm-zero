e1234b29b15247380e4930e62e3a44629cf9380a
UHeeJoon <yhjyhj3855@gmail.com>
2026-04-15 10:50:16 +0900

    feat: 15. 3Sum

---
+++ leetcode/15. 3Sum/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 중복된 첫 번째 숫자 건너뛰기
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i]; // nums[i] + nums[left] + nums[right] == 0 이므로, nums[left] + nums[right] == -nums[i]
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // 중복된 두 번째/세 번째 숫자 건너뛰기
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else { // sum > target
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
